30a28166fc8828e88fc2ba2af598318c1ac60903
wyjeon <61363233+wyjeon@users.noreply.github.com>
2021-01-24 19:27:02 +0900

    Update Node.js의 패키지 매니저와 PM2

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ function templateHTML(title, list, body) {
   <!doctype html>
   <html>
   <head>
-    <title>WEB1 - ${title}</title>
+    <title>WEB - ${title}</title>
     <meta charset="utf-8">
   </head>
   <body>
-    <h1><a href="/">WEB</a></h1>
+    <h1><a href="/">WEB2</a></h1>
     ${list}
     ${body}
   </body>
