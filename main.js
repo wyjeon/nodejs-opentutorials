9f1e0da80600bfd321ab3b92d726497258c3e2ce
wyjeon <wyjeon.info@gmail.com>
2021-01-25 15:13:36 +0900

    Update 글수정 - 수정 링크 생성

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var url = require("url");
 var qs = require("querystring");
 
-function templateHTML(title, list, body) {
+function templateHTML(title, list, body, control) {
   return `
   <!doctype html>
   <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB2</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${control}
     ${body}
   </body>
   </html>
@@ -45,7 +45,8 @@ var app = http.createServer(function (request, response) {
         var template = templateHTML(
           title,
           list,
-          `<h2>${title}</h2>${description}`
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`
         );
         response.writeHead(200);
         response.end(template);
@@ -61,7 +62,8 @@ var app = http.createServer(function (request, response) {
             var template = templateHTML(
               title,
               list,
-              `<h2>${title}</h2>${description}`
+              `<h2>${title}</h2>${description}`,
+              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
             );
             response.writeHead(200);
             response.end(template);
@@ -86,7 +88,8 @@ var app = http.createServer(function (request, response) {
             <input type="submit">
           </p>
           </form>
-        `
+        `,
+        ""
       );
       response.writeHead(200);
       response.end(template);
